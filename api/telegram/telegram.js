4f1591a4d59aee773fd2a82e34bd89db3fccd814
jordidh <jordi.dh@gmail.com>
2021-03-10 10:47:28 +0100

    bug:actualitzem configuracio bot de telegram per evitar error 502 i que es quedi penajt

--- api/telegram/telegram.js
+++ api/telegram/telegram.js
@@ -95,6 +95,13 @@ const TEXT = {
 
 const bot = new TeleBot({
     token: config.TELEGRAM.TOKEN,
+    polling: { // Optional. Use polling.
+        interval: 3000, // Optional. How often check updates (in ms).
+        timeout: 0, // Optional. Update polling timeout (0 - short polling).
+        limit: 100, // Optional. Limits the number of updates to be retrieved.
+        retryTimeout: 5000 // Optional. Reconnecting timeout (in ms).
+        //proxy: 'http://username:password@yourproxy.com:8080' // Optional. An HTTP proxy to be used.
+    },
     usePlugins: ['askUser', 'namedButtons'],
     pluginConfig: {
         namedButtons: {
